850ccb56ffebc3be7ac8e59cf2d28c7427e854ae
Asher <ashmenzer@gmail.com>
2023-04-27 11:06:39 +0300

    add tests to get_valid_piece_takes

--- test_knight.py
+++ test_knight.py
@@ -80,3 +80,82 @@ def test_get_valid_peaceful_moves_with_other_pieces() -> None:
     assert sorted(k1.get_valid_peaceful_moves(game)) == [(1, 5), (2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
 
 
+def test_get_valid_piece_takes_empty():
+    game = game_state()
+    k1 = Knight('n', 3, 4, Player.PLAYER_1)
+    game.board = board = [
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY]
+    ]
+    game.board[3][4] = k1
+    assert sorted(k1.get_valid_piece_takes(game)) == []
+
+
+def test_get_valid_piece_takes():
+    game = game_state()
+    k1 = Knight('n', 3, 4, Player.PLAYER_1)
+    k2 = Knight('n', 1, 3, Player.PLAYER_2)
+    game.board = board = [
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY]
+    ]
+    game.board[3][4] = k1
+    game.board[1][3] = k2
+    assert sorted(k1.get_valid_piece_takes(game)) == [(1, 3)]
+
+
+def test_get_valid_piece_takes_with_same_player_piece():
+    game = game_state()
+    k1 = Knight('n', 3, 4, Player.PLAYER_1)
+    k2 = Knight('n', 1, 3, Player.PLAYER_1)
+    game.board = board = [
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY]
+    ]
+    game.board[3][4] = k1
+    game.board[1][3] = k2
+    assert sorted(k1.get_valid_piece_takes(game)) == []
+
+
