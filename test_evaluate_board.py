414ae292d85d76d6db176555ad7551900a9021fa
Asher <ashmenzer@gmail.com>
2023-04-27 11:07:55 +0300

    add integretion test

---
+++ test_evaluate_board.py
@@ -0,0 +1,36 @@
+import ai_engine
+from Piece import Knight
+import ai_engine
+from chess_engine import game_state
+from enums import Player
+
+
+def test_evaluate_board():
+    ai = ai_engine.chess_ai()
+    game = game_state()
+    k1 = Knight('n', 3, 4, Player.PLAYER_1)
+    k2 = Knight('n', 1, 3, Player.PLAYER_2)
+    k3 = Knight('n', 1, 5, Player.PLAYER_1)
+    game.board = board = [
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY],
+        [Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY, Player.EMPTY,
+         Player.EMPTY]
+    ]
+    game.board[3][4] = k1
+    game.board[1][3] = k2
+    game.board[1][5] = k3
+    assert ai.evaluate_board(game,Player.PLAYER_1) == -30
+
